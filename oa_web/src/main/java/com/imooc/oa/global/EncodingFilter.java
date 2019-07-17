f29955f94204dea15168102a26139782e62fe2ed
xuanjinya <1067998193@qq.com>
2019-07-17 17:25:07 +0800

    配置过滤器

---
+++ oa_web/src/main/java/com/imooc/oa/global/EncodingFilter.java
@@ -0,0 +1,38 @@
+package com.imooc.oa.global;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author: Yaking
+ * @Date: 2019-07-17 17:16
+ * @Describe: 过滤器
+ */
+public class EncodingFilter implements Filter {
+    private String encoding = "utf-8";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        /*初始化方法：读取初始化参数*/
+        if (filterConfig.getInitParameter("encoding") != null) {
+            encoding = filterConfig.getInitParameter("encoding");
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        /*过滤方法*/
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        request.setCharacterEncoding(encoding);
+        response.setCharacterEncoding(encoding);
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
